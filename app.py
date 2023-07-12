50cc140a39c0d93d65db92af69e94b54027bf556
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-07-12 22:14:24 +0530

    Reduce sample name redundancy

--- app.py
+++ app.py
@@ -28,17 +28,18 @@ if test:
         print(e)
 
     # Add a sample document in the collection (don't add if already exists)
-    if collection.count_documents({'name': 'John Doe'}) == 0:
-        collection.insert_one({'name': 'John Doe'})
+    name = 'John Doe'
+    if collection.count_documents({'name': name}) == 0:
+        collection.insert_one({'name': name})
         print("✅ Added a sample document in the collection.")
 
     # Retrieve the sample document added
-    result = collection.find_one({'name': 'John Doe'})
+    result = collection.find_one({'name': name})
     print(result)
     print("✅ Retrieved the sample document.")
 
     # Delete the sample document added
-    # collection.delete_one({'name': 'John Doe'})
+    # collection.delete_one({'name': name})
 
 
 @app.route('/')
