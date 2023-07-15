9d66f51d22f6cdf12d18adda3716928d8a3ed31a
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-07-15 12:04:16 +0530

    Add sample data & render

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import json
+from datetime import datetime
 
 import os
 from dotenv import load_dotenv
@@ -39,13 +41,32 @@ if test:
     print("✅ Retrieved the sample document.")
 
     # Delete the sample document added
-    # collection.delete_one({'name': name})
+    collection.delete_one({'name': name})
+    print("✅ Deleted the sample document.")
+
+
+def formatData(posts_dict):
+
+    for post in posts_dict:
+        # Convert date from string to datetime object
+        post['date'] = datetime.strptime(post['date'], '%Y-%m-%d')
+
+        # Convert date to string in the format DD MMM YYYY
+        post['date_str'] = post['date'].strftime('%d %b %Y')
+
+    return posts_dict
 
 
 @app.route('/')
 def index():
 
-    return render_template('index.html')
+    with open('./static/json/sample_posts.json', 'r') as file:
+        data = json.load(file)
+        posts_dict = data['posts']
+
+    posts_dict = formatData(posts_dict)
+
+    return render_template('index.html', posts_dict=posts_dict)
 
 
 if __name__ == '__main__':
